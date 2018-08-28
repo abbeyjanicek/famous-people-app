e9716aedbe34e7e6158f289dacdeb0fd27a34e36
Abbey Janicek <abbey.janicek@gmail.com>
2018-08-28 15:53:48 -0500

    Finished base mode except for Submit button.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,6 @@ import './App.css';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = { nameInput: '', roleInput: '' };
-    this.handleNameChange = this.handleNameChange.bind(this);
-  }
-
-  handleNameChange(event) {
-    console.log('Name was changed!', event.target.value);
-    this.setState({ nameInput: event.target.value })
-  }
-
-  handleRoleChange = (event) => {
-    console.log('Role was changed!', event.target.value);
-    this.setState({ roleInput: event.target.value });
-  };
-
   render() {
     return (
       <div className="App">
@@ -29,12 +13,6 @@ class App extends Component {
         <p className="App-intro">
           Enter the name of a famous person and the role for which they are famous.
         </p>
-        <div> 
-          <input onChange={this.handleNameChange} />
-          <input onChange={this.handleRoleChange} />
-          <br />
-          {this.state.nameInput} is famous for {this.state.roleInput}
-        </div>
         <Person />
       </div>
     );
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class Person extends Component {
+
+    constructor() {
+        super();
+        this.state = { nameInput: '', roleInput: '' };
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        console.log('Name was changed!', event.target.value);
+        this.setState({ nameInput: event.target.value })
+    }
+
+    handleRoleChange = (event) => {
+        console.log('Role was changed!', event.target.value);
+        this.setState({ roleInput: event.target.value });
+    };
+
     render() {
         return (
+
             <div>
-                <h1>Name:</h1>
+                Name: <input onChange={this.handleNameChange} />
+                <br />
+                Role: <input onChange={this.handleRoleChange} />
+                <br />
+                {this.state.nameInput} is famous for {this.state.roleInput}
             </div>
+
+
         );
     }
 }
